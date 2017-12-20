7cdf418a3637a487fe6e6177f4b0b24f6031877a
tiago <tiago_honorio2010@hotmail.com>
2017-12-20 10:07:47 -0200

    start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsuarioComponent } from './usuario/usuario.component';
+import { TarefaComponent } from './tarefa/tarefa.component';
+import { RouterModule, Routes } from '@angular/router';
+import {UsuarioService} from './usuario/usuario.service';
+import {TarefaService} from './tarefa/tarefa.service';
 
+
+const appRoutes:Routes = [
+  {path:'home',component:AppComponent},
+  {path:'usuario',component:UsuarioComponent},
+  {path:'tarefa',component:TarefaComponent}
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsuarioComponent,
+    TarefaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes),
+    HttpModule
   ],
-  providers: [],
+  providers: [UsuarioService, TarefaService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
